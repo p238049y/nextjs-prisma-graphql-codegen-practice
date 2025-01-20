ab26f2c23c28bf8fdf79dc00c2a849b8234ced19
p238049y <p238049y@gmail.com>
2025-01-20 23:13:07 +0900

    [update] マイグレーションファイルを修正

---
+++ prisma/migrations/20250120132605_/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Session` table. All the data in the column will be lost.
+  - The required column `id` was added to the `Session` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE "Session" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Session_pkey" PRIMARY KEY ("id");
---
+++ prisma/migrations/20250120133242_v0_0_1_add_init_tables/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - The primary key for the `Session` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Session` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Session" DROP CONSTRAINT "Session_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
