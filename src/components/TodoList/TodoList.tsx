f9b5ba796298a51a9693cfe4b5b15e0d9e0cccda
p238049y <p238049y@gmail.com>
2025-01-20 23:26:40 +0900

    [update] TodoListのコンポーネントを追加

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -10,7 +10,7 @@ import { FC, FormEvent, useEffect, useState } from 'react'
 export const TodoList: FC = () => {
   const [todoTitle, setTodoTitle] = useState('')
   const [todos, setTodos] = useState<TodosQuery['todos']>([])
-  const { loading, error, data } = useTodosQuery()
+  const { loading, error, data, refetch } = useTodosQuery()
   const [addTodoMutation] = useAddTodoMutation()
   const [updateTodoMutation] = useUpdateTodoMutation()
   const [deleteTodoMutation] = useDeleteTodoMutation()
@@ -32,13 +32,10 @@ export const TodoList: FC = () => {
   }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    console.log('addedTodo')
-    // e.preventDefault()
+    e.preventDefault()
 
     const { data } = await addTodoMutation({ variables: { title: todoTitle } })
     const addedTodo = data?.addTodo
-
-    console.log('addedTodo', addedTodo)
 
     if (!addedTodo) {
       return
@@ -46,6 +43,42 @@ export const TodoList: FC = () => {
 
     setTodos([addedTodo, ...todos])
     setTodoTitle('')
+    await refetch()
+  }
+
+  const handleChange = async (
+    todoId: string,
+    completed: boolean
+  ): Promise<void> => {
+    const { data } = await updateTodoMutation({
+      variables: { todoId, completed },
+    })
+    const updateTodo = data?.updateTodo
+
+    if (!updateTodo) {
+      return
+    }
+
+    const updatedTodos = todos.map((t) =>
+      t?.id === updateTodo.id ? updateTodo : t
+    )
+    setTodos(updatedTodos)
+  }
+
+  const handleDelete = async (todoId: string): Promise<void> => {
+    const isOk = confirm('削除しますか？')
+    if (!isOk) {
+      return
+    }
+
+    const { data } = await deleteTodoMutation({ variables: { todoId } })
+    const todo = data?.deleteTodo
+    if (!todo) {
+      return
+    }
+
+    const deletedTodos = todos.filter((t) => t?.id !== todo.id)
+    setTodos(deletedTodos)
   }
 
   return (
@@ -60,6 +93,25 @@ export const TodoList: FC = () => {
         />
         <button className="bg-gray-200 p-2">追加</button>
       </form>
+      <ul className="mt-5">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id} className={`${todo.completed && 'line-through'}`}>
+              <span>
+                {todo.completed ? '✅' : '👀'} {todo.title}
+              </span>{' '}
+              <input
+                className="cirsor-pointer"
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => handleChange(todo.id, todo.completed)}
+              />
+              <span> / </span>
+              <button onClick={() => handleDelete(todo.id)}>🗑️</button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
